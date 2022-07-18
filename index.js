d0b9bea66e60479b8322b3c1c7010c6a9c2eaa28
Glitch (night-like-pot) <none>
2022-07-18 20:54:10 +0000

    🕶🌥 Checkpoint ./index.js:36770902/3148 ./src/events/ready.js:36770902/305

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const { Client, Collection } = require('discord.js');
 const { messageCreate } = require('./src/events/messageCreate');
+const { ready } = require('./src/events/ready');
 const { readdirSync } = require('fs');
-const client = new Client({ intents: 32767 }) 
+const client = new Client({ intents: 32767 })
+
+ready(client);
+
 client.commands = new Collection();
 
 const folders = readdirSync('commands');
-
 for (let folder of folders) {
-  const files = readdirSync('commands/' + folder);
-  
- for (let file of files) {
- const pull = require('./commands/' + folder + '/' + file);
-    
-  client.commands.set(pull.name, pull);
+  const files = readdirSync('commands/' + folder);
+    for (let file of files) {
+    const pull = require('./commands/' + folder + '/' + file);
+    client.commands.set(pull.name, pull);
+    messageCreate(client, pull);
   }
 }
-
 client.login(process.env.token);
 require('./src/util');
---
+++ src/events/ready.js
@@ -0,0 +1,3 @@
+module.exports.ready = (client) => {
+  console.log(`${client.user.username} Ready !!`);
+};
