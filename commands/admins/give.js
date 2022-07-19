d67dd14762e739f1581a758fad32015305416425
Glitch (night-like-pot) <none>
2022-07-19 11:10:31 +0000

    🎠🏐 Checkpoint ./commands/admins/give.js:36770902/727 ./database.json:36770902/3

--- commands/admins/give.js
+++ commands/admins/give.js
@@ -11,15 +11,16 @@ module.exports = {
          if (!user) return message.reply({embeds: [new MessageEmbed().setColor(client.color).setDescription(`**⁉️ | ${message.author.username}, I can't find ${args[1]}!**`)], allowedMentions: { repliedUser: false}});                      
          if (user.bot) return message.reply({content: `:thinking:  | **${message.author.username}**, bots do not have credits!`, allowedMentions: { repliedUser: false }});
          const credits = data.get(`credits_${user.id}`) || 0; 
-         if(!args[1].includes("-") {
-         const amount = args[2].replace("-", '')
-          if (remove && credits < args[2]) return message.reply(`**❌ I Can't remove credits**`).catch(() => {});
-          if (add) data.add(`credits_${user.id}`, 1);
-          if (remove) data.subtract(`credits_${message.author.id}`, args[2]);            
-         
-        return message.channel.send("done..");
+         if(args[2].includes("-")) {
+         let amount = args[2].replace("-", '')
+         if (credits < amount) return message.reply(`**❌ I Can't remove credits**`).catch(() => {});
+         data.subtract(`credits_${user.id}`, Number(1));                      
+         return message.channel.send("done remove..");
+        } else {
+          data.add(`credits_${user.id}`, parseInt(args[2]));
+         return message.channel.send("done give..");
+      } 
         break;
-       } 
     }
   },
 };
